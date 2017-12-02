2b68a607ec0677f505edd990d1ce15e94bb30f39
Adam R. Nelson <adam@nels.onl>
2017-12-01 20:46:02 -0500

    Fix version number

--- src/ReservedNames.ts
+++ src/ReservedNames.ts
@@ -2,7 +2,7 @@
 export const prefix = '$'
 
 export const primitiveModule = 'jaspr.primitive'
-export const version = '0.1.171124'
+export const version = '0.1.171201'
 
 export const closure = prefix + 'closure'
 export const code = prefix + 'code'
